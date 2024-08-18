990c795fab8c75598c19e595c5961e66cece7da9
EsraaSalah28 <30927332+EsraaSalah28@users.noreply.github.com>
2024-08-18 17:18:02 +0300

    Time: 0 ms (100.00%), Space: 41.4 MB (14.46%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -30,23 +30,5 @@ class Solution {
     return root;
 }
 
-public static List<Integer> dfs(TreeNode root) {
-    List<Integer> list = new ArrayList<>();
-    if (root == null) return list;
-    
-    // Use a queue to traverse the tree level by level (BFS).
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-    
-    while (!queue.isEmpty()) {
-        TreeNode node = queue.poll();
-        list.add(node.val);
-        
-        // Add left and right children to the queue for further traversal.
-        if (node.left != null) queue.add(node.left);
-        if (node.right != null) queue.add(node.right);
-    }
-    
-    return list;
-}
+
 }
