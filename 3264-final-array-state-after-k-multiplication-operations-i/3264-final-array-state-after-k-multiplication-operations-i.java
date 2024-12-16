41a6b9acaa88cba9e2d231dcbd151ad4718f9a79
EsraaSalah28 <30927332+EsraaSalah28@users.noreply.github.com>
2024-12-17 00:08:10 +0300

    Time: 3 ms (34.22%), Space: 45 MB (21.40%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -1,17 +1,27 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-       int n = nums.length;
-        for(int i=0;i<k;i++){
-            int minIndex=0;
-         for(int j=0;j<n;j++)
-         {
-             if(nums[j]<nums[minIndex])
-             {
-                 minIndex=j;
-             }
+     PriorityQueue<int[]> heap = new PriorityQueue<>((a,b)->{
+         int value=Integer.compare(a[0],b[0]);
+         if(value==0){
+          return Integer.compare(a[1],b[1]);     
          }
-            nums[minIndex]*=multiplier;
+         return value;
+     });
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            heap.offer(new int []{nums[i],i});
+            
         }
-        return nums;
+        while(k-->0)
+        {
+            int [] list=heap.poll();
+            int index= list[1];
+            nums[index]*= multiplier;
+                       heap.offer(new int[] { nums[index], index });
+ 
+        }
+                return nums;
+
     }
 }
