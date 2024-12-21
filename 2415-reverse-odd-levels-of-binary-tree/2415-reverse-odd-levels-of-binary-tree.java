0b1c33efefea9d8e74d56b29b5026c4db34f3233
EsraaSalah28 <30927332+EsraaSalah28@users.noreply.github.com>
2024-12-21 22:38:54 +0300

    Time: 10 ms (33.47%), Space: 48.7 MB (43.57%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -15,21 +15,38 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        traverseDFS (root.left,root.right,0);
-        return root;
-    }
-    private void traverseDFS (TreeNode left ,TreeNode right , int level)
-    {
-        if(left ==null ||right==null)
-            return;
-        if(level %2==0){
-            int temp = left.val;
-            left.val=right.val;
-            right.val= temp;
-        }
-        traverseDFS(left.left,right.right,level+1);
-        traverseDFS(left.right,right.left,level+1);
-
+            if(root==null)
+                return null;
         
+        Queue<TreeNode> queue= new LinkedList<>();
+        queue.add(root); int level=0;
+        while(!queue.isEmpty())
+        {
+             int size=queue.size();
+            List<TreeNode> currentLevelNodes= new ArrayList<>();
+            
+            for(int i=0 ;i<size;i++)
+            {
+                TreeNode node = queue.poll();
+                currentLevelNodes.add(node);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            if(level %2==1)
+            {
+                int left=0 , right=currentLevelNodes.size()-1;
+                while(left<right)
+                {
+                    int temp = currentLevelNodes.get(left).val;
+                    currentLevelNodes.get(left).val= currentLevelNodes.get(right).val;
+                    currentLevelNodes.get(right).val=temp;
+                    left++;
+                    right--;
+                }
+            }
+            level++;
+        }
+
+        return root;
     }
 }
