8ebab463e2989fa3c7f9491b930227e83255c7f4
EsraaSalah28 <30927332+EsraaSalah28@users.noreply.github.com>
2025-01-05 00:15:20 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,29 +1,26 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int n = s.length();
-        boolean[][] prefix = new boolean[n][26]; // Prefix array
-        boolean[][] suffix = new boolean[n][26]; // Suffix array
-
-        // Build prefix array
-        for (int i = 1; i < n; i++) {
-            System.arraycopy(prefix[i - 1], 0, prefix[i], 0, 26);
-            prefix[i][s.charAt(i - 1) - 'a'] = true;
-        }
-
-        // Build suffix array
-        for (int i = n - 2; i >= 0; i--) {
-            System.arraycopy(suffix[i + 1], 0, suffix[i], 0, 26);
-            suffix[i][s.charAt(i + 1) - 'a'] = true;
-        }
-
-        // Count unique palindromic subsequences
+     int n = s.length();
         Set<String> uniquePalindromes = new HashSet<>();
+
+        // Iterate through each character as the middle character
         for (int mid = 1; mid < n - 1; mid++) {
             char midChar = s.charAt(mid);
 
-            for (char c = 'a'; c <= 'z'; c++) {
-                if (prefix[mid][c - 'a'] && suffix[mid][c - 'a']) {
-                    uniquePalindromes.add("" + c + midChar + c);
+            // Check for matching characters on the left and right
+            Set<Character> leftSet = new HashSet<>();
+            for (int left = 0; left < mid; left++) {
+                if (s.charAt(left) == midChar) {
+                    leftSet.add(s.charAt(left));
+                }
+            }
+
+            // For each character in the left set, check the right side
+            for (Character c : leftSet) {
+                for (int right = mid + 1; right < n; right++) {
+                    if (s.charAt(right) == c) {
+                        uniquePalindromes.add("" + c + midChar + c);
+                    }
                 }
             }
         }
