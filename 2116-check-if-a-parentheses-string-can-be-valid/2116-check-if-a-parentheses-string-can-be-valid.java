2800f80ed2f7b47c594f2a6ef2bac82afd585517
EsraaSalah28 <30927332+EsraaSalah28@users.noreply.github.com>
2025-01-13 00:26:47 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -1,22 +1,41 @@
 class Solution {
-    public static boolean canBeValid(String s, String locked) {
 
-        Stack<Character> stack = new Stack();
+    public boolean canBeValid(String s, String locked) {
         int n = s.length();
         if (n % 2 != 0)
             return false;
+        Stack<Integer> openBrackets = new Stack<>();
+        Stack<Integer> unlocked = new Stack<>();
         for (int i = 0; i < n; i++) {
-            if (!stack.isEmpty()) {
-                char c = stack.pop();
-                if (c == s.charAt(i)) {
-                    if (locked.charAt(i) == '1' && c == '1') {
-                        return false;
-                    }
+            if (locked.charAt(i) == '0') {
+                unlocked.push(i);
+            } else if (s.charAt(i) == '(') {
+                openBrackets.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!openBrackets.isEmpty()) {
+                  openBrackets.pop();
+                }else if(!unlocked)
+                {
+                    unlocked.pop();
+                }else
+                {
+                    return true;
                 }
             }
-            stack.push(s.charAt(i));
-
         }
-        return true;
+        while (
+                    !openBrackets.empty() &&
+                            !unlocked.empty() &&
+                            openBrackets.peek() < unlocked.peek()
+            ) {
+                openBrackets.pop();
+                unlocked.pop();
+            }
+
+            if (!openBrackets.empty()) {
+                return false;
+            }
+
+            return true;
     }
 }
