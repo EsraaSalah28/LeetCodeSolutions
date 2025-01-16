283c04310738df5cd9ca3b4d4976f0253db7eef2
EsraaSalah28 <30927332+EsraaSalah28@users.noreply.github.com>
2025-01-16 16:58:43 +0200

    Time: 1 ms (100%), Space: 61 MB (80.17%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.java
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.java
@@ -1,30 +1,25 @@
 class Solution {
 
     public static int xorAllNums(int[] nums1, int[] nums2) {
-        // Compute the XOR of all elements in nums1 and nums2
-        int xor1 = 0;
-        int xor2 = 0;
-
-        for (int num : nums1) {
-            xor1 ^= num;
+        int xor1=0;
+        int xor2=0;
+        int result=0;
+        for(int n : nums1)
+        {
+            xor1^=n;
         }
-
-        for (int num : nums2) {
-            xor2 ^= num;
+         for(int n : nums2)
+        {
+            xor2^=n;
         }
-
-        // Result variable
-        int result = 0;
-
-        // Check the parity of the lengths
-        if (nums1.length % 2 == 1) {
-            result ^= xor2; // nums2 contributes if nums1 has an odd length
+        if(nums1.length%2==1)
+        {
+            result^=xor2;
         }
-
-        if (nums2.length % 2 == 1) {
-            result ^= xor1; // nums1 contributes if nums2 has an odd length
+         if(nums2.length%2==1)
+        {
+            result^=xor1;
         }
-
         return result;
     }
 
